92495c7334d29790b6cadff948cc330fef2a7d2c
shkgn24 <39944653+shkgn24@users.noreply.github.com>
2018-06-13 22:27:30 +0900

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+//cin.sync_with_stdio(false);
+
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define FOR_RE(i,a,b) for(int i = (a); i > (b); --i)
+#define rep(i,n) FOR(i,0,n)
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> Vector;
+typedef vector<Vector>	DVector;
+int n;
+
+int main(int argc, char const *argv[])
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cout << "hello" << endl;
+    return 0;
+}
